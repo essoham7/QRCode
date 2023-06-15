7c01212835dac93cf699bdd0984c1a5b07607ecb
Essoham SIBABI <richessoham@gmail.com>
2023-06-15 00:42:39 +0000

    feat(ui) add title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className="container mx-auto px-4 py-16 sm:px-6 lg:px-8">
         <div className="text-center">
           <h1 className="text-4xl tracking-tight font-extrabold  sm:text-5xl md:text-6xl">
-            Bienvenue sur mon site
+            Bienvenue sur Esso QR code generator
           </h1>
           <p className="mt-8  mb-8 w-full mx-auto text-2xl text-gray-900">
           Bienvenue sur notre site de génération de QR Codes ! Créez facilement des codes QR personnalisés pour vos liens, 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <img className="h-8 w-8" src="/path/to/logo.png" alt="Logo" />
+              {/* <img className="h-8 w-8" src="/path/to/logo.png" alt="Logo" /> */}
             </div>
             <div className="hidden md:block">
               <div className="ml-10 flex items-baseline space-x-4">
