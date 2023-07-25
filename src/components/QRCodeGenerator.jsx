ac8f27e3679643ccfefc83279367ba0fa24b7c76
Essoham SIBABI <richessoham@gmail.com>
2023-07-25 00:33:52 +0000

    feat(ui): extension

--- src/components/QRCodeGenerator.jsx
+++ src/components/QRCodeGenerator.jsx
@@ -29,9 +29,9 @@ const QRCodeGenerator = () => {
     setSize(parseInt(e.target.value));
   };
 
-  // const handleExtensionChange = (e) => {
-  //   setExtension(e.target.value);
-  // };
+  const handleExtensionChange = (e) => {
+    setExtension(e.target.value);
+  };
 
   const handleDownloadQRCode = () => {
     const canvas = document.querySelector('canvas');
@@ -100,15 +100,15 @@ const QRCodeGenerator = () => {
       </div>
 
       </div>
-      {/* <div className="mb-4 flex justify-center">
+      <div className="mb-4 flex justify-center">
         <label className="mb-2 tracking-tight sm:text-3xl md:text-2xl text-gray-900">Extension :</label>
         <select value={extension} onChange={handleExtensionChange} className="w-full p-2 border border-gray-300 rounded-md">
           <option value="png">PNG</option>
-          <option disabled value="eps">EPS</option>
+          {/* <option disabled value="eps">EPS</option>
           <option disabled value="pdf">PDF</option>
-          <option disabled value="svg">SVG</option>
+          <option disabled value="svg">SVG</option> */}
         </select>
-      </div> */}
+      </div>
       <div className="mb-4 flex justify-center img-container">
         <QRCode
           value={text}
